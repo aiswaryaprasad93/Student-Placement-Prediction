8a3e17921838dd38a5c5eb081a746a08b9f51aee
aiswaryaprasad93 <aiswaryaprasad8547@gmail.com>
2026-06-21 23:22:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,27 +13,36 @@ mba = st.number_input("MBA Percentage")
 
 if st.button("Predict"):
        student = pd.DataFrame([[
-        1,
-        ssc,
-        0,
-        hsc,
-        0,
-        2,
-        degree,
-        2,
-        1,
-        85,
-        1,
-        mba
-    ]], columns=[
-        'gender','ssc_p','ssc_b','hsc_p','hsc_b',
-        'hsc_s','degree_p','degree_t',
-        'workex','etest_p','specialisation','mba_p'
-    ])
+    1,
+    ssc,
+    0,
+    hsc,
+    0,
+    2,
+    degree,
+    2,
+    1,
+    85,
+    1,
+    mba
+]], columns=[
+    'gender',
+    'ssc_p',
+    'ssc_b',
+    'hsc_p',
+    'hsc_b',
+    'hsc_s',
+    'degree_p',
+    'degree_t',
+    'workex',
+    'etest_p',
+    'specialisation',
+    'mba_p'
+])
 
-        prediction = model.predict(student)[0]
+prediction = model.predict(student)[0]
 
-       if prediction == 1:
-        st.success("Prediction: Placed")
-       else:
-        st.error("Prediction: Not Placed")
+if prediction == 1:
+    st.success("Prediction: Placed")
+else:
+    st.error("Prediction: Not Placed")
