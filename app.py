f55fef47c891b2b0f7e0ca2e86f5f679b38e2a95
aiswaryaprasad93 <aiswaryaprasad8547@gmail.com>
2026-06-21 23:06:36 +0530

    Updated app to use ML model prediction
    Added model loading and prediction logic using joblib and pandas.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 
 import streamlit as st
+import pandas as pd
+import joblib
+model = joblib.load("placement_model.pkl")
 
 st.title("Student Placement Prediction")
 
@@ -9,7 +12,28 @@ degree = st.number_input("Degree Percentage")
 mba = st.number_input("MBA Percentage")
 
 if st.button("Predict"):
-    if ssc >= 60 and hsc >= 60 and degree >= 60 and mba >= 60:
+       student = pd.DataFrame([[
+        1,
+        ssc,
+        0,
+        hsc,
+        0,
+        2,
+        degree,
+        2,
+        1,
+        85,
+        1,
+        mba
+    ]], columns=[
+        'gender','ssc_p','ssc_b','hsc_p','hsc_b',
+        'hsc_s','degree_p','degree_t',
+        'workex','etest_p','specialisation','mba_p'
+    ])
+
+    prediction = model.predict(student)[0]
+
+    if prediction == 1:
         st.success("Prediction: Placed")
     else:
         st.error("Prediction: Not Placed")
